f9a1d41d8b98bebc638029d3c8129c678df6e831
JoaoPauloCabral <50843677+JoaoPauloCabral@users.noreply.github.com>
2022-04-15 19:13:06 -0300

    add nav function

--- js/script.js
+++ js/script.js
@@ -2,3 +2,10 @@
 const copyrightYearEl = document.querySelector('.footer__year');
 const currentYear = new Date().getFullYear();
 copyrightYearEl.textContent = currentYear;
+
+// Closes the nav
+document.querySelector('.navigation__list').addEventListener('click', () => {
+    document
+        .querySelector('.navigation__button')
+        .dispatchEvent(new MouseEvent('click'));
+});
